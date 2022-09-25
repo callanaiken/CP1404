25367b8c10fa8707f420775ded370bf4b5d2f948
callanaiken <callan.aiken@my.jcu.edu.au>
2022-09-25 22:47:15 +1000

    Amend with functions to determine comment and add random number

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,15 +2,30 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = int(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-elif score < 100:
-    print("Excellent")
-else:
-    print("Invalid score")
+
+def main():
+    score = int(input("Enter score: "))
+    print(f"You're score is: {determine_comment(score)}")
+
+    random_number = random.randint(0, 100)
+    print(f"The random score is: {random_number}")
+    print(f"Random score is: {determine_comment(random_number)}")
+
+
+def determine_comment(score):
+    if score < 0:
+        comment = "Invalid score"
+    elif score < 50:
+        comment = "Bad"
+    elif score < 90:
+        comment = "Passable"
+    elif score < 100:
+        comment = "Excellent"
+    else:
+        comment = "Invalid score"
+    return comment
+
+
+main()
