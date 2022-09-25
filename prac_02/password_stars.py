5f75f8db79fcd8cfda37fd0f0a18ca81a2ff04df
callanaiken <callan.aiken@my.jcu.edu.au>
2022-09-25 21:47:15 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,10 +2,24 @@
 CP1404 Prac 02, Password input with error checking for length.
 """
 
-minimum_password_length = 10
 
-password = input("Password: ")
-while len(password) < minimum_password_length:
-    print("Invalid password, character length must be 10 or greater.")
+def main():
+    minimum_password_length = 10
+
+    password = get_password(minimum_password_length)
+    display_password(password)
+
+
+def display_password(password):
+    print("*" * len(password))
+
+
+def get_password(minimum_password_length):
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < minimum_password_length:
+        print("Invalid password, character length must be 10 or greater.")
+        password = input("Password: ")
+    return password
+
+
+main()
